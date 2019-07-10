0f1b6739300d826aa2c40bf36cb9554c53bbc2c1
hitmachut <hitmachut@SHAKED-22>
2019-07-10 22:08:09 +0300

    check

--- DTO/UsersDTO.cs
+++ DTO/UsersDTO.cs
@@ -8,8 +8,6 @@ namespace DTO
 {
     public class UsersDTO
     {
-       
-
         public int id { get; set; }
         public string name { get; set; }
         public string password { get; set; }
